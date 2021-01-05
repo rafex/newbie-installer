96fbb2a68ff4fee4e9536c8b0f3a683a8af7cfe1
Raúl González <raulegleza@gmail.com>
2021-01-05 03:48:40 -0600

    agregando mejoras al archivo de configuracion de gogs

--- bin/serv/vcs/gogs/gogs-install.sh
+++ bin/serv/vcs/gogs/gogs-install.sh
@@ -94,15 +94,35 @@ function create_config_gogs () {
   RUN_USER = ${GOGS_USER}
   RUN_MODE = prod
 
-  [database]
+[database]
   DB_TYPE  = sqlite3
   HOST     = 127.0.0.1:3306
   PATH     = ${INSTALLATION_PATH_GOGS}/data/sqlite/gogs.db
 
-  [repository]
+[repository]
   ROOT = ${INSTALLATION_PATH_GOGS}/data/repository
 
-  [server]
+[repository.editor]
+  ; List of file extensions that should have line wraps in the CodeMirror editor.
+  ; Separate extensions with a comma.
+    LINE_WRAP_EXTENSIONS = .txt,.md,.markdown,.mdown,.mkd
+  ; Valid file modes that have a preview API associated with them, such as "/api/v1/markdown".
+  ; Separate values by commas. Preview tab in edit mode won't show if the file extension doesn't match.
+    PREVIEWABLE_FILE_MODES = markdown
+
+[repository.upload]
+  ; Whether to enable repository file uploads.
+  ENABLED = true
+  ; The path to temporarily store uploads (content under this path gets wiped out on every start).
+  TEMP_PATH = /tmp
+  ; File types that are allowed to be uploaded, e.g. "image/jpeg|image/png". Leave empty to allow any file type.
+  ALLOWED_TYPES =
+  ; The maximum size of each file in MB.
+  FILE_MAX_SIZE = 200
+  ; The maximum number of files per upload.
+  ;  MAX_FILES = 5
+
+[server]
   DOMAIN           = ${MY_IP}
   HTTP_PORT        = ${GOGS_PORT}
   ROOT_URL         = http://${MY_IP}:${GOGS_PORT}/
@@ -110,32 +130,95 @@ function create_config_gogs () {
   SSH_PORT         = 22
   START_SSH_SERVER = false
   OFFLINE_MODE     = true
+  ENABLE_GZIP 	   = true
+  ; Landing page for non-logged users, can be "home" or "explore"
+  LANDING_PAGE 	   = explore
 
-  [mailer]
+
+[mailer]
   ENABLED = false
 
-  [service]
+[service]
   REGISTER_EMAIL_CONFIRM = false
   ENABLE_NOTIFY_MAIL     = false
   DISABLE_REGISTRATION   = false
   ENABLE_CAPTCHA         = true
   REQUIRE_SIGNIN_VIEW    = true
 
-  [picture]
+[picture]
   DISABLE_GRAVATAR        = true
   ENABLE_FEDERATED_AVATAR = false
 
-  [session]
+[session]
+  ; The session provider, either "memory", "file", or "redis".
   PROVIDER = file
+  ; The configuration for respective provider:
+  ; - memory: does not need any config yet
+  ; - file: session file path, e.g. `data/sessions`
+  ; - redis: network=tcp,addr=:6379,password=macaron,db=0,pool_size=100,idle_timeout=180
+  PROVIDER_CONFIG = data/sessions
+  ; The cookie name to store the session identifier.
+  COOKIE_NAME = i_like_gogs
+  ; Whether to set cookie in HTTPS only.
+  COOKIE_SECURE = true
+  ; The GC interval in seconds for session data.
+  GC_INTERVAL = 3600
+  ; The maximum life time in seconds for a session.
+  MAX_LIFE_TIME = 86400
+  ; The cookie name for CSRF token.
+  CSRF_COOKIE_NAME = _csrf
 
-  [log]
+[log]
   MODE      = file
   LEVEL     = Info
   ROOT_PATH = /var/log/gogs
 
-  [security]
+[security]
   INSTALL_LOCK = true
   SECRET_KEY   = ${RANDOM_ALPHANUMERIC}
+  ; Auto-login remember days
+  LOGIN_REMEMBER_DAYS = 7
+  COOKIE_USERNAME = gogs_awesome
+  COOKIE_REMEMBER_NAME = gogs_incredible
+  ; Reverse proxy authentication header name of user name
+  REVERSE_PROXY_AUTHENTICATION_USER = X-WEBAUTH-USER
+
+[auth]
+  ; The valid duration of activate code in minutes.
+  ACTIVATE_CODE_LIVES = 180
+  ; The valid duration of reset password code in minutes.
+  RESET_PASSWORD_CODE_LIVES = 180
+  ; Whether to require email confirmation for adding new email addresses.
+  ; Enable this option will also require user to confirm the email for registration.
+  REQUIRE_EMAIL_CONFIRMATION = false
+  ; Whether to disallow anonymous users visiting the site.
+  REQUIRE_SIGNIN_VIEW = false
+  ; Whether to disable self-registration. When disabled, accounts would have to be created by admins.
+  DISABLE_REGISTRATION = false
+  ; Whether to enable captcha validation for registration
+  ENABLE_REGISTRATION_CAPTCHA = true
+
+  ; Whether to enable reverse proxy authentication via HTTP header.
+  ENABLE_REVERSE_PROXY_AUTHENTICATION = true
+  ; Whether to automatically create new users for reverse proxy authentication.
+  ENABLE_REVERSE_PROXY_AUTO_REGISTRATION = true
+  ; The HTTP header used as username for reverse proxy authentication.
+  REVERSE_PROXY_AUTHENTICATION_HEADER = X-WEBAUTH-USER
+
+[attachment]
+  ; Whether issue and pull request attachments are enabled. Defaults to `true`
+  ENABLED = true
+  ; Comma-separated list of allowed file extensions (`.zip`), mime types (`text/plain`) or wildcard type (`image/*`, `audio/*`, `video/*`). Empty value or `*/*` allows all types.
+  ALLOWED_TYPES = .docx,.gif,.gz,.jpeg,.jpg,.log,.pdf,.png,.pptx,.txt,.xlsx,.zip
+  ; Max size of each file. Defaults to 4MB
+  MAX_SIZE = 4
+  ; Max number of files per upload. Defaults to 5
+  MAX_FILES = 5
+  ; Storage type for attachments, `local` for local disk or `minio` for s3 compatible
+  ; object storage service, default is `local`.
+  STORAGE_TYPE = local
+
+
 EOF
   has_sudo
   sudo cp -v $TMP_PATH_GOGS/app.ini.newbie /etc/gogs/config/app.ini
